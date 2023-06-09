f58b3b2d3232d17a32835cf97ff683d016e7e925
Adam Szymanowski <adam.szym@yahoo.pl>
2023-06-09 07:44:03 +0200

    Chapter 2 listings (examples).

---
+++ sources/listing-2-22.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+struct ClockOfTheLongNow {
+    ClockOfTheLongNow() {
+        year = 2019;
+    }
+    int get_year() {
+        return year;
+    }
+private:
+    int year;
+};
+
+int main() {
+    ClockOfTheLongNow clock;
+    printf("Default year: %d", clock.get_year());
+}
