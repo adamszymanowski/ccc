134afa06c2256d8dfc025aec55e1794627e28fd7
Adam Szymanowski <adam.szym@yahoo.pl>
2023-06-12 09:16:46 +0200

    Proper path resolving for source.

--- debug.bat
+++ debug.bat
@@ -1,8 +1,9 @@
 @echo off
 
 set target_exe="builds\%1.exe"
+set project_root=%~dp0
 if exist %target_exe% (
-    devenv /edit sources\%1.cpp /debugexe %target_exe%
+    devenv /edit %project_root%sources\%1.cpp /debugexe %target_exe%
 ) else (
     echo Target executable %target_exe% does not exist! Cannot debug.
 )
